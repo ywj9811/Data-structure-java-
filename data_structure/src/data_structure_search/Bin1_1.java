570f1bad22832e0a075c546be0607eefea2a92ba
ywj9811 <ywj9811@naver.com>
2022-03-25 02:12:19 +0900

    정리

--- data_structure/src/data_structure_search/Bin1_1.java
+++ data_structure/src/data_structure_search/Bin1_1.java
@@ -5,29 +5,29 @@ import java.util.Scanner;
 
 public class Bin1_1 {
 	public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-		
+		Scanner sc = new Scanner(System.in);
+
 		System.out.print("요솟수 : ");
 		int n = sc.nextInt();
 		int[] x = new int[n]; //n크기의 배열 생성
-		
+
 		System.out.println("오름차순으로 입력하시오.");
-		
+
 		System.out.print("x[0] : ");
 		x[0] = sc.nextInt();
-		
+
 		for(int i = 1; i < n; i++) {
 			do {
 				System.out.print("x[" + i + "] : ");
 				x[i] = sc.nextInt();
 			} while(x[i] < x[i-1]); //바로앞의 값보다 작을경우 다시 입력 (오름차순 맞춰야하니)
 		}
-		
+
 		System.out.print("검색할 값 : ");
 		int key = sc.nextInt();
-		
+
 		int idx = Arrays.binarySearch(x, key);
-		
+
 		if(idx < 0)
 			System.out.println("없습니다 삽입포인트는 : " + (-idx - 1) + "입니다.");
 		else
