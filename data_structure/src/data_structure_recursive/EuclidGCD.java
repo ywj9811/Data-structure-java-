83a8f41c2e3a2d322f4171ff1c4df97f94e7932e
ywj9811 <ywj9811@naver.com>
2022-03-31 23:52:08 +0900

    recursive

---
+++ data_structure/src/data_structure_recursive/EuclidGCD.java
@@ -0,0 +1,23 @@
+package data_structure_recursive;
+
+import java.util.Scanner;
+
+public class EuclidGCD {
+	static int gcd(int x, int y) {
+		if(y == 0)
+			return x;
+		else
+			return gcd(y, x%y);
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("두 정수의 최대공약수를 구합니다");
+		System.out.print("1 : ");
+		int x = sc.nextInt();
+		System.out.print("2 : ");
+		int y = sc.nextInt();
+		
+		System.out.println("최대 공약수 : " + gcd(x, y));
+	}
+}
